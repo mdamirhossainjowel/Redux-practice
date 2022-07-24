acf6c0ae70a0abc23f664453a359d09c32cde083
md amir hossain jowel <mdamirhossainjowel@gmail.com>
2022-07-24 16:13:16 +0600

    middlewire Updatet

--- middlewire.js
+++ middlewire.js
@@ -1,38 +1,34 @@
-const {createStore}=require('redux');
+const { createStore } = require("redux");
 
-
-const ADDUSER='ADDUSER'
+const ADDUSER = "ADDUSER";
 
 const userState = {
-  users: ['Amir'],
+  users: ["Amir"],
 };
 
 const userCounter = (user) => {
   return {
     type: ADDUSER,
-    payload:user,
+    payload: user,
   };
 };
-
 
 const userReducer = (state = userState, action) => {
   switch (action.type) {
     case ADDUSER:
       return {
-        users:[...state.users, action.payload],
+        users: [...state.users, action.payload],
       };
-   
+
     default:
       return state;
   }
 };
 
+const store = createStore(userReducer);
 
-const store=createStore(userReducer);
+store.subscribe(() => {
+  console.log(store.getState());
+});
 
-store.subscribe(()=>{
-      console.log(store.getState());
-})
-
-
-store.dispatch(userCounter("JOWEL"))
+store.dispatch(userCounter("JOWEL"));
